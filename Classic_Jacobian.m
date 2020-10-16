5f4d00a5f625396cd6273c34d6e47bbfc5de09df
AB-22 <72051401+AB-22@users.noreply.github.com>
2020-10-16 21:52:55 +0300

    Delete Classic_Jacobian.m

--- Classic_Jacobian.m
+++
@@ -1,8 +0,0 @@
-function J=Classic_Jacobian(q)
-a2=5;
-d1=3;
-J=[-(q(3)+a2)*cosd(q(2))*sind(q(1)) -(q(3)+a2)*cosd(q(1))*sind(q(2)) cosd(q(1))*cosd(q(2));...
-    (q(3)+a2)*cosd(q(1))*cosd(q(2)) -(q(3)+a2)*sind(q(1))*sind(q(2)) sind(q(1))*cosd(q(2));...
-               0                            (q(3)+a2)*cosd(q(2))           sind(q(2))     ];
-           
-end
